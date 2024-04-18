f35bfca64c4d2f85f1a798ab843ff1ac77187647
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-04-18 12:45:52 +0200

    [add] input

--- index.php
+++ index.php
@@ -7,8 +7,11 @@
   <div id="app">
 
       <div class="container">
+
+        <h2 class="text-center titolo mt-3">I tuoi dischi preferiti</h2>
         <div class="row row-cols-3  my-5">
 
+        
           <div 
           v-for="(item, index) in disk"
           :key="index"
@@ -27,6 +30,48 @@
             </div>
           </div>
 
+        </div>
+
+        <h2 class="text-center titolo">Aggiungi un nuovo disco</h2>
+        <div class="text-white d-flex justify-content-center m-5">
+          
+
+          <div class="d-flex flex-column mx-2">
+            <label for="">Title</label>
+            <input 
+              v-model.trim="newDisk.title"
+              placeholder="Aggiungi un titolo"
+              type="text" name="">
+          </div>
+
+          <div class="d-flex flex-column mx-2">
+            <label for="">Author</label>
+            <input 
+              v-model.trim="newDisk.author"
+              placeholder="Aggiungi un autore"
+              type="text" name="">
+          </div>
+
+          <div class="d-flex flex-column mx-2">
+            <label for="">Year</label>
+            <input 
+              v-model.trim="newDisk.year"
+              placeholder="Aggiungi l'anno"
+              type="text" name="">
+          </div>
+
+          <div class="d-flex flex-column mx-2">
+            <label for="">Genre</label>
+            <input 
+              v-model.trim="newDisk.genre"
+              placeholder="Aggiungi il genere"
+              type="text" name="">
+          </div>
+
+        </div>
+        
+        <div class="d-flex justify-content-center align-items-center mb-5">
+          <button type="button" class="btn btn-success btn-sm">Aggiungi</button>
         </div>
       </div>
 
--- script.js
+++ script.js
@@ -5,7 +5,14 @@ createApp ({
   data () {
     return {
       apiUrl: "server.php",
-      disk: []
+      disk: [],
+      newDisk: {
+        title: "",
+        author: "",
+        year: "",
+        poster: "https://m.media-amazon.com/images/I/61WnkDvkqUL._AC_SL1048_.jpg",
+        genre: ""
+      }
     }
   },
 
@@ -16,6 +23,19 @@ createApp ({
           console.log(result.data);
           this.disk= result.data;
         })
+    },
+
+    addNewTask () {
+      const data = new FormData();
+      data.append('newDiskTitle', this.newDisk.title);
+      data.append('newDiskAuthor', this.newDisk.author);
+      data.append('newDiskYear', this.newDisk.year);
+      data.append('newDiskGenre', this.newDisk.genre);
+
+      axios.post(this.apiUrl, data)
+        then(result =>{
+          console.log(result.data);
+      })
     }
 
   },
